273dd0e136dc548411e7a021443d1418485a43ac
Denis Erfurt <denis.erfurt@gmail.com>
2016-08-05 19:39:30 +0200

    massive refactoring for sol base ds

---
+++ spec/script.sol
@@ -0,0 +1,16 @@
+contract Script {
+  // bunch of events which direct the interaction
+  event exportNumber(string name, uint number);
+  event exportObject(string name, address addr);
+
+  event setCalls(bool flag);
+  event setOrigin(address origin);
+
+  event shUint(bytes input, uint result);
+
+  modifier static {
+    setCalls(true);
+    _
+    setCalls(false);
+  }
+}
