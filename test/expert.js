a2ef3d45a4b66996f7c8d02a8046397bd3ff8b32
Denis Erfurt <denis.erfurt@gmail.com>
2016-09-01 15:59:03 +0200

    refactor expert object

---
+++ test/expert.js
@@ -0,0 +1,16 @@
+/* global it, describe */
+'use strict';
+
+var assert = require('chai').assert;
+
+describe('Expert', function () {
+
+  var classes = {
+    A: {
+      bytecode: "12345",
+      interface: "[]"
+    }
+  };
+  before(() => {});
+  
+});
